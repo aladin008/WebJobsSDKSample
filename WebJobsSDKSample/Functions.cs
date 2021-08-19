76a32dbbeb30b12a4f2e586103a56e35949c7066
qing lu <aladin008@hotmail.com>
2021-08-19 16:46:52 -0500

    Add project files.

---
+++ WebJobsSDKSample/Functions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebJobsSDKSample
+{
+    public class Functions
+    {
+        static HttpClient client = new HttpClient();
+        static public void GetProductAsync(ILogger logger)
+        {
+            string path = "https://productsapp20210818221653.azurewebsites.net/api/Products";
+
+            var response = client.GetAsync(path).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string message=DateTime.Now.ToString()+' '+response.Content;
+                logger.LogInformation(message);
+                // await response.Content.ReadAsAsync();
+            }
+            return;
+        }
+
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+
+    }
+}
